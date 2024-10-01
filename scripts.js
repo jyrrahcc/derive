2a731d8712dffbaf3609ca9c98e2c00031219974
jyrrahcc <jyrrahcc@gmail.com>
2024-10-01 20:47:27 +0800

    polish2

--- scripts.js
+++ scripts.js
@@ -87,3 +87,25 @@ window.onscroll = function() {
     }
     
     document.addEventListener('DOMContentLoaded', setupIntersectionObserver);
+
+
+    // Example starter JavaScript for disabling form submissions if there are invalid fields
+(function () {
+    'use strict'
+
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    var forms = document.querySelectorAll('.needs-validation')
+
+    // Loop over them and prevent submission
+    Array.prototype.slice.call(forms)
+        .forEach(function (form) {
+            form.addEventListener('submit', function (event) {
+                if (!form.checkValidity()) {
+                    event.preventDefault()
+                    event.stopPropagation()
+                }
+
+                form.classList.add('was-validated')
+            }, false)
+        })
+})()
